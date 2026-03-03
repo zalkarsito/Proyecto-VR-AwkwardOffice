3d402e04e873d0272cf9e2e353f9d95032b53f08
zalkarsito <zalkarsito@gmail.com>
2026-03-03 13:55:39 +0100

    Limpieza y añadir los confetti

--- Assets/script/TrashCollector.cs
+++ Assets/script/TrashCollector.cs
@@ -1,13 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class TrashCollector : MonoBehaviour
 {
 
     public GameManager Manager;
+    public GameObject confettipos;
+    public GameObject confettiPrefab;
+    private GameObject confetti;
+
+    public ParticleSystem ps;
 
     private void Start()
     {
         Manager = Object.FindFirstObjectByType<GameManager>();
+        ps.Stop();
+        
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -16,8 +24,35 @@ public class TrashCollector : MonoBehaviour
         if (other.gameObject.CompareTag("Trash"))
         {
             Debug.Log("Entra en el comparador de trash");
+
             other.gameObject.SetActive(false);
 
+            ps.Play();
+            StartCoroutine(Espera());
+            /*if (confetti == null)
+            {
+                confetti = Instantiate(confettiPrefab, confettipos.transform.position, confettipos.transform.rotation);
+                confetti.transform.localScale = Vector3.one;
+                confetti.SetActive(true);
+                StartCoroutine(Espera());
+                
+            }
+
+            else
+            {
+                confetti.SetActive(true);
+                StartCoroutine(Espera());
+                
+            }*/
+            
+
         }
+    }
+
+    IEnumerator Espera()
+    {
+        yield return new WaitForSeconds(2f);
+        //confetti.SetActive(false);
+        ps.Stop();
     }
 }
